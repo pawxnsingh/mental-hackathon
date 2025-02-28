e8c5144fd473e72e0c1ea003626519d808dc4250
pawan singh dogra <psdograofficial@gmail.com>
2025-02-28 20:45:40 +0530

    random fucked up

--- app/models/thread.ts
+++ app/models/thread.ts
@@ -1,22 +1,12 @@
 import mongoose from "mongoose";
-import { v4 as uuidv4 } from 'uuid';
 
 const chatThreadSchema = new mongoose.Schema({
-  // Use UUID as the document _id
-  _id: {
-    type: String,
-    default: uuidv4,
-  },
   title: {
     type: String,
     required: true,
     maxlength: 255, // enforce a maximum length of 255 characters
     // You can include a comment for help text if needed
     // help: "Thread title or subject"
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
   }
 });
 
